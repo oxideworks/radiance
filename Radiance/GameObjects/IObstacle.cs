25829ae91de207c1d79c98920beaa6dbd6a418f4
Arseniy Kucher <beeffallo@gmail.com>
2018-12-02 22:32:27 +0200

    Obstacle

---
+++ Radiance/GameObjects/IObstacle.cs
@@ -0,0 +1,16 @@
+﻿using Radiance.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radiance.GameObjects
+{
+    public interface IObstacle
+    {
+        bool Contains(Vector point);
+        bool Intersects(IObstacle obstacle);
+
+    }
+}
--- Radiance/GameObjects/Obstacle.cs
+++ Radiance/GameObjects/Obstacle.cs
@@ -7,17 +7,30 @@ using System.Threading.Tasks;
 
 namespace Radiance.GameObjects
 {
-    public class Obstacle : Polymer
+    public class Obstacle : Polymer, IObstacle
     {
-        public Obstacle(List<Vector> points) : base(Sort(points))
+        public Obstacle(List<Vector> points)
+            : base(Sort(points))
+        {
+
+        }
+
+        public Obstacle(Vector[] points)
+            : this(points.ToList())
         {
 
         }
 
         private static List<Vector> Sort(List<Vector> points)
         {
-            // Do sorting here.
-            return points;
+            var origin = points
+                .OrderBy(p => p.Y)
+                .ThenBy(p => p.X)
+                .First();
+            var ordered = points
+                .OrderByDescending(p => Math.Atan2(p.Y - origin.Y, p.X - origin.X))
+                .ToList();
+            return ordered;
         }
     }
 }
