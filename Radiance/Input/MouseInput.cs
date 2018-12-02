4c87a3b58e5ae1303f2201deed72181daa22ccfc
Arseniy Kucher <beeffallo@gmail.com>
2018-12-02 13:23:15 +0200

    Input

---
+++ Radiance/Input/MouseInput.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+
+namespace Radiance.Input
+{
+    public class MouseInput
+    {
+        public MouseInput()
+        {
+            Window.Current.CoreWindow.PointerMoved += MouseMoved;
+        }
+
+        private void MouseMoved(CoreWindow sender, PointerEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Radiance/Primitives/Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radiance.Primitives
+{
+    public class Rectangle
+    {
+        public Rectangle(float side) : this(side, side)
+        {
+
+        }
+
+        public Rectangle(float width, float height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public Rectangle(double width, double height) : this((float)width, (float)height)
+        {
+
+        }
+
+        public float Width { get; private set; }
+        public float Height { get; private set; }
+    }
+}
