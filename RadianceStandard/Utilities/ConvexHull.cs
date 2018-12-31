ea5273042629871a59d48a3334e72e9c235cd973
Arseniy Kucher <beeffallo@gmail.com>
2018-12-31 14:06:10 +0200

    Fixed some problems

--- RadianceStandard/Utilities/ConvexHull.cs
+++ RadianceStandard/Utilities/ConvexHull.cs
@@ -5,25 +5,22 @@ using System.Linq;
 
 namespace RadianceStandard.Utilities
 {
-    public class ConvexHull
+    public class ConvexHuller
     {
-        #region Ctors
-        public ConvexHull(IHardenedPolymer polymer)
+        #region Methods
+        public IHardenedPolymer ComputeConvexHull(IHardenedPolymer polymer)
         {
-            if (polymer.Count < 3) throw new InvalidNumberOfNodesException("Ну вы децыбел.");
-            Hull = GrahamScan(polymer);
+            if (polymer.Count < 3)
+                throw new InvalidNumberOfNodesException("Ну вы децыбел.");
+            return GrahamScan(polymer);
         }
-        #endregion
-
-        #region Props
-        public IHardenedPolymer Hull;
         #endregion
 
         #region privates
         private IHardenedPolymer GrahamScan(IHardenedPolymer polymer)
         {
             var angleSorter = new PolymerSorter();
-            var origin = polymer.OrderBy(p => p.X).ThenBy(p => p.Y).First();
+            var origin = polymer.OrderBy(p => p.Y).ThenBy(p => p.X).First();
             var ordered = angleSorter.Sort(polymer, origin);
             var stack = new Stack<Vector>();
             stack.Push(ordered[0]);
@@ -45,7 +42,7 @@ namespace RadianceStandard.Utilities
             var (x1, y1) = p0.ToTuple();
             var (x2, y2) = p1.ToTuple();
             var (x3, y3) = pi.ToTuple();
-            var loc = (y2 - y1) * (x3 - x2) - (y3 * y2) * (x2 - x1);
+            var loc = (y2 - y1) * (x3 - x2) - (y3 - y2) * (x2 - x1);
             if (loc < 0) return true;
             else return false;
         }
